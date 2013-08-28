39c32341d6e9f5887788c4ee3869e6a32f51c193
jhutto <jhutto@backgroundchecks.com>
2013-08-28 14:20:20 -0400

    better, not good.

--- pokey.js
+++ pokey.js
@@ -2,17 +2,22 @@ var prompt = require("./prompt/lib/prompt.js");
 
 var suits = new Array('C','D','H','S');
 var ranks = new Array('2','3','4','5','6','7','8','9','J','Q','K','A');
-var cards = make2dArray(suits,ranks);
-
+var cards = make2dArray(ranks,suits);
+console.log(cards);
 prompt.start();
 prompt.get(['Hands'], function(err, result){
 	if(err){return onErr(err);}
 	console.log('well....')	
 });
 
-function make2dArray(primary, secondary){
-	var returnArray = new Array()
-	primary.forEach(returnArray.push(primary[]));
+function make2dArray(primary, secondary){	
+	var returnArray = new Array();
+	for(var i=0;i<primary.length;i++){
+		for(var ct=0;ct<secondary.length;ct++){
+			returnArray.push(primary[i] + secondary[ct]);
+		}
+	}
+	return returnArray;	
 };
 
 function onErr(err){
